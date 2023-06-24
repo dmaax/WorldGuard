a27606a4ac02dd33c58afa21e23afeddc92d9398
dmaax <dudzcool@gmail.com>
2023-06-24 20:05:00 -0300

    fix: Update gradle settings to allow Jenkins builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,7 @@ include("worldguard-libs:core")
 include("worldguard-core")
 //include("worldguard-libs:bukkit")
 include("worldguard-bukkit")
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
